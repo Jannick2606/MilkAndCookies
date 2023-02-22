50cc58d30666511f6672fe7592af08f38f2dd50a
Jannick Sørensen <jann6665@zbc.dk>
2023-02-22 12:34:21 +0100

    Add project files.

---
+++ MilkAndCookies/Controllers/MilkshakesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MilkAndCookies.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MilkshakesController : ControllerBase
+    {
+        [HttpGet]
+        public string Get(string? favoriteMilkshake)
+        {
+            CookieOptions co = new CookieOptions();
+            co.HttpOnly = true;
+            co.Secure = true;
+            co.Expires = DateTimeOffset.MaxValue;
+            if (favoriteMilkshake == null) return "";
+            Response.Cookies.Append("favoriteMilkshake", favoriteMilkshake, co);
+            return favoriteMilkshake;
+        }
+        [HttpGet]
+        [Route("[action]")]
+        public string GetFromCookie()
+        {
+            if (Request.Cookies["favoriteMilkshake"] == null) return "Unknown";
+            return Request.Cookies["favoriteMilkshake"]!;
+            
+        }
+    }
+}
---
+++ MilkAndCookies/Controllers/OtherController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MilkAndCookies.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OtherController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            if (Request.Cookies["favoriteMilkshake"] == null) return "No cookie found";
+            return Request.Cookies["favoriteMilkshake"]!;
+        }
+        [HttpDelete]
+        public void Delete()
+        {
+            if (Request.Cookies["favoriteMilkshake"] != null) Response.Cookies.Delete("favoriteMilkshake");
+        }
+
+    }
+}
